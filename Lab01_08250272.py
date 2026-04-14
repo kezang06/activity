a7c10c1a7419306853a741cd18c3c12faff202c4
Kzaa Decheyy <08250272sce@rub.edu.bt>
2026-04-14 22:50:35 +0530

    .\Lab01_08250272.py

--- Lab01_08250272.py
+++ Lab01_08250272.py
@@ -1,43 +1,50 @@
-students_list = []        
-grades_set = set()        
-students_dict = {}        
+# Student Information Management System
 
-students_list.append("Kezang")
-grades_set.add("k")
-students_dict["Kezang"] = {"Age": 23, "Grade": "K"}
+# create list and dictionary
+students = [] # save student names in list
+student_data = {} # save student details (name,age and grade) in dict
 
-students_list.append("Dechen")
-grades_set.add("D")
-students_dict["Dechen"] = {"Age": 22, "Grade": "D"}
+# add student name,age and grade
+name = input("Enter name: ") # enter the name of the students
+age = input("Enter age: ")  #enter the age of the students
+student_grade = input("Enter grade: ") # enter the grade of the students
 
-students_list.append("Jigme")
-grades_set.add("J")
-students_dict["Jigme"] = {"Age": 21, "Grade": "J"}
-
-students_list.append("Yoezer")
-grades_set.add("Y")
-students_dict["Yoezer"] = {"Age": 23, "Grade": "Y"}
-
-search_name = input("Enter the name of the student to search: ")
-if search_name in students_list:
-    print(f"Student found! Name: {search_name}, Age: {students_dict[search_name]['Age']}, Grade: {students_dict[search_name]['Grade']}")
+# check if student already exists or not
+if name in student_data: # to check student exist or not
+    print("already exists.") #print "already exists."
 else:
-    print("Student not found.")
+    students.append(name) # add new student names
+    student_data[name] = {"age": age, "grade": student_grade} #add details(age and grade)
+    print("Student added.") #print "student added."
 
-remove_name = input("Enter the name of the student to remove or press Enter to skip: ")
-if remove_name in students_list:
-    remove_grade = students_dict[remove_name]["Grade"]
-    
-    students_list.remove(remove_name)
-    del students_dict[remove_name]
+# display student records
+print(" Student Details") # print "Student Details"
 
-    if remove_grade not in [info["Grade"] for info in students_dict.values()]:
-        grades_set.remove(remove_grade)
-
-    print("Student removed successfully.")
-    print("Students available along with their details:", students_dict)
-    print("Just available student names:", students_list)
-    print("Just the grades available:", grades_set)
-
+if student_data: # check if student records in dict
+    for s in student_data: 
+        print("Name:", s) # print student name
+        print("Age:", student_data[s]["age"]) #print age
+        print("Grade:", student_data[s]["grade"]) #print grade
 else:
-    print("Student not found.")
+    print("Student not found.") #print "No student records found"
+
+# Name searching
+name_s = input("Enter name to search: ") #enter name to search
+
+if name_s in student_data: # check if the name is in the dict
+    print("Record found:") #print "Record found"
+    print("Name:", name_s) # print the name of the student
+    print("Age:", student_data[name_s]["age"]) #print the age of the student
+    print("Grade:", student_data[name_s]["grade"]) #print the grade of the students
+else:
+    print("Student not found.") #print "Student not found"
+
+# remove students 
+remove_name = input("Enter name to remove: ") # enter name to remove
+
+if remove_name in student_data: #check if the name is in the dict or not
+    students.remove(remove_name) #remove name
+    del student_data[remove_name] #delete info in dict
+    print("Student record deleted.") #print "Student record deleted"
+else: #check if the name in dict 
+    print("Student not found.") #print "Student not found." 
